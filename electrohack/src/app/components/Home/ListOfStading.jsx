1a4ec03d6d09ce22ce186b812804b027451b6aff
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-12 18:47:32 -0300

    si

--- electrohack/src/app/components/Home/ListOfStading.jsx
+++ electrohack/src/app/components/Home/ListOfStading.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
 
 const ListOfStading = () => {
   const [products, setProducts] = useState(false);
@@ -13,67 +15,49 @@ const ListOfStading = () => {
       });
   });
   return (
-    <div>
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide mt-5 "
-        data-ride="carousel"
-      >
-        <ol className="carousel-indicators">
-          {products &&
-            products.map((item, index) => {
-              return (
-                <li
-                  data-target="#carouselExampleCaptions"
-                  data-slide-to={index}
-                  key={index}
-                ></li>
-              );
-            })}
-        </ol>
-        {/* <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img src="..." className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img src="..." className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img src="..." className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <a
-                className="carousel-control-prev"
-                href="#carouselExampleCaptions"
-                role="button"
-                data-slide="prev"
-              >
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a
-                className="carousel-control-next"
-                href="#carouselExampleCaptions"
-                role="button"
-                data-slide="next"
-              >
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a> */}
+    <div className="container mt-5">
+      <div className="row mt-5">
+        <h3 className="mt-5 col-12 text-center mb-3">Nuestros productos destacados</h3>
+        <div className="col-6 mt-5">
+          <Carousel>
+            {products &&
+              products.map((item, index) => {
+                return (
+                  <Carousel.Item interval={2000}>
+                    <img
+                      className="d-block w-100 image_carrousel img-fluid "
+                      src={item.pictures[0]}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3 className=" bg-dark">{item.name}</h3>
+                      <p className=" bg-dark">{item.description}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                );
+              })}
+          </Carousel>
+        </div>
+        <div className="col-6 mt-5">
+          <Carousel>
+            {products &&
+              products.map((item, index) => {
+                return (
+                  <Carousel.Item interval={2000}>
+                    <img
+                      className="d-block w-100 image_carrousel img-fluid "
+                      src={item.pictures[0]}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3 className=" bg-dark">{item.name}</h3>
+                      <p className=" bg-dark">{item.description}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                );
+              })}
+          </Carousel>
+        </div>
       </div>
     </div>
   );
