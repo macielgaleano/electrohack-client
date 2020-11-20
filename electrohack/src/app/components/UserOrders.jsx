c26b921a7a636e9947f8495464e92889a3605ab1
Martin Magnone <tinchomagnone23@gmail.com>
2020-11-20 12:09:23 -0300

    userOrders OK

--- electrohack/src/app/components/UserOrders.jsx
+++ electrohack/src/app/components/UserOrders.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import "./Order.css";
 // import { Link } from "react-router-dom";
 import Nav from "./Nav/Nav";
@@ -24,44 +25,58 @@ export default function UserOrders() {
   console.log("store user", store.user);
   console.log("order", orders);
 
-  return (
-    <>
-      <Nav />
+  if (orders.length === 0) {
+    return (
+      <>
+        <Nav />
+        <h2 className="text-center">Aún no has realizado compras</h2>
+        <h3 className="mt-4 d-flex justify-content-center">
+          Visita nuestra tienda
+          <Link to="/" className="pl-2">
+            AQUÍ
+          </Link>
+        </h3>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Nav />
+        <div className="container">
+          <h2 className="text-center mb-3">Mis órdenes</h2>
 
-      <div className="container">
-        <h2 className="text-center mb-3">Mis órdenes</h2>
-
-        {orders.map((order) => {
-          return (
-            <div className="box">
-              {order.products.map((product) => {
-                console.log("multip", product.quantity * product.price);
-                return (
-                  <p>
-                    <strong>Artículo:</strong> {product.name} -{" "}
-                    <strong>Cantidad:</strong> {product.quantity}
-                  </p>
-                );
-              })}
-              <div className="row">
-                <div className="col-md-6">
-                  <p>
-                    <strong>Estado de la orden: </strong>
-                    {order.state}
-                  </p>
-                </div>
-                <div className="col-md-6">
-                  <div className="d-flex justify-content-end pr-3">
+          {orders.map((order) => {
+            return (
+              <div className="box">
+                {order.products.map((product) => {
+                  console.log("multip", product.quantity * product.price);
+                  return (
                     <p>
-                      <strong>TOTAL:</strong> ${Math.round(order.total)}
+                      <strong>Artículo:</strong> {product.name} -{" "}
+                      <strong>Cantidad:</strong> {product.quantity}
                     </p>
+                  );
+                })}
+                <div className="row">
+                  <div className="col-md-6">
+                    <p>
+                      <strong>Estado de la orden: </strong>
+                      {order.state}
+                    </p>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="d-flex justify-content-end pr-3">
+                      <p>
+                        <strong>TOTAL:</strong> ${Math.round(order.total)}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
