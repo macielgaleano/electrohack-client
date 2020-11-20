f1c8d91abd4b9983f6a11a746d39033137672e64
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-20 14:51:51 -0300

    si

--- electrohack/src/app/components/AboutUs/AboutUs.jsx
+++ electrohack/src/app/components/AboutUs/AboutUs.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import "./AboutUs.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default function AboutUs() {
   return (
     <div className="about-body">
       <Nav />
-      <div className="container">
-        <div className="row ">
-          <div className="col-7">
-            <h2 className="pb-5 pt-5">Presentación del proyecto</h2>
-            <p>
-              ELECTROHACK es un e-commerce que realizamos como proyecto final
-              del coding bootcamp de Hack-Academy. Para la parte del Back-End y
-              base de datos utilizamos las siguientes tecnologías:
+      <div className="poster-bg m-0 p-0 d-flex align-items-center">
+        <div className="container">
+          <div className="col-12">
+            <h1 className="pb-4 pt-5 mt-5 text-white">Presentación del proyecto</h1>
+            <p className="lead text-white">
+              ELECTROHACK es un e-commerce que realizamos como proyecto final del coding
+              bootcamp de Hack-Academy. Para la parte del Back-End y base de datos
+              utilizamos las siguientes tecnologías:
             </p>
           </div>
-          <div className="col-5 border-rounded" style={{ marginTop: "140px" }}>
+          <div className="row mt-5">
+            <div className="col-1 mr-2-0 alert bg-primary text-center">React</div>
+            <div className="col-1 mr-2 alert bg-primary text-center">Express</div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row ">
+          <div className="col-7"></div>
+          <div className="col-6 border-rounded" style={{ marginTop: "140px" }}>
+            <img src={window.location.origin + "/mobile.PNG"} />
+          </div>
+          <div className="col-6 border-rounded" style={{ marginTop: "140px" }}>
             <img src={window.location.origin + "/mobile.PNG"} />
           </div>
         </div>
@@ -24,15 +38,13 @@ export default function AboutUs() {
           <div className="col-9">
             <h2 className="mb-4">Quienes Somos</h2>
             <p>
-              Somos un grupo de 3 Desarrolladores Web Jr. que nos encontramos
-              trabajando en este proyecto con el fin de que el mismo pueda ser
-              testeado por nuestros futuros entrevistadores y utilizarlo como
-              vidriera.
+              Somos un grupo de 3 Desarrolladores Web Jr. que nos encontramos trabajando
+              en este proyecto con el fin de que el mismo pueda ser testeado por nuestros
+              futuros entrevistadores y utilizarlo como vidriera.
             </p>
             <p className="pb-5">
-              En un tiempo de algo mas de 2 semanas obtuvimos este producto con
-              el cual estamos conformes pero seguiremos actualizando los
-              próximos días.
+              En un tiempo de algo mas de 2 semanas obtuvimos este producto con el cual
+              estamos conformes pero seguiremos actualizando los próximos días.
             </p>
           </div>
           <div className="col-4">
@@ -45,11 +57,11 @@ export default function AboutUs() {
               <div className="card-body">
                 <h5 className="card-title">Sebastián César</h5>
                 <p className="card-text">
-                  Personalmente fue mi primer experiencia en un proyecto
-                  completo pasando por Back-End, Front-End y base de datos.
+                  Personalmente fue mi primer experiencia en un proyecto completo pasando
+                  por Back-End, Front-End y base de datos.
                   <br />
-                  Quedé muy conforme y con muchas ganas de perfeccionar y pulir
-                  muchas cosas en futuros proyectos
+                  Quedé muy conforme y con muchas ganas de perfeccionar y pulir muchas
+                  cosas en futuros proyectos
                 </p>
                 <a href="https://www.linkedin.com/in/sebastian-cesar">
                   Visitame en LinkedIn
@@ -63,8 +75,8 @@ export default function AboutUs() {
               <div className="card-body">
                 <h5 className="card-title">Maciel Galeano</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Some quick example text to build on the card title and make up the bulk
+                  of the card's content.
                 </p>
                 <a href="#" className="btn btn-primary">
                   Go somewhere
@@ -78,8 +90,8 @@ export default function AboutUs() {
               <div className="card-body">
                 <h5 className="card-title">Martin Magnone</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Some quick example text to build on the card title and make up the bulk
+                  of the card's content.
                 </p>
                 <a href="#" className="btn btn-primary">
                   Go somewhere
